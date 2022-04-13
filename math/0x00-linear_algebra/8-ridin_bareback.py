e47a0cee4b784f24d3c18cdd6147d3e02bca0ff3
Sarolus <cactussius.primus@gmail.com>
2022-04-13 15:33:34 +0200

    Update task 8

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -15,21 +15,21 @@ def mat_mul(mat1: list, mat2: list):
     Returns:
         list: Return the result of the multiplication
     """
-    if len(mat1) != len(mat2):
+    try:
+        result = []
+
+        for row in range(len(mat1)):
+            result.append([])
+
+            for column in range(len(mat2[0])):
+                # Initialize the result matrix.
+                result[row].append(0)
+
+                for element in range(len(mat2)):
+                    # Multiply the row of mat1 with the column of mat2.
+                    result[row][column] += mat1[row][element] * \
+                        mat2[element][column]
+
+        return result
+    except:
         return None
-
-    result = []
-
-    for row in range(len(mat1)):
-        result.append([])
-
-        for column in range(len(mat2[0])):
-            # Initialize the result matrix.
-            result[row].append(0)
-
-            for element in range(len(mat2)):
-                # Multiply the row of mat1 with the column of mat2.
-                result[row][column] += mat1[row][element] * \
-                    mat2[element][column]
-
-    return result
