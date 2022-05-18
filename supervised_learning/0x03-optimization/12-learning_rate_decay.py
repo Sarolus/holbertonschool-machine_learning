1db5ae0a1abaa107b011231deabd90899daeb22c
Sarolus <cactussius.primus@gmail.com>
2022-05-18 16:43:01 +0200

    Make pycodestyle compliant

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -11,4 +11,8 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
         Creates the operation to perform the learning rate decay
     """
 
-    return tf.train.inverse_time_decay(alpha, global_step, decay_step, decay_rate, staircase=True)
+    return tf.train.inverse_time_decay(alpha,
+                                       global_step,
+                                       decay_step,
+                                       decay_rate,
+                                       staircase=True)
