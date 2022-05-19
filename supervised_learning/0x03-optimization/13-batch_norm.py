399418669bc5779d7fcfabb5caf05a32846f0849
Sarolus <cactussius.primus@gmail.com>
2022-05-19 14:31:39 +0200

    Add batch normalization task

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 """
-    Creates the operation to perform the batch normalization
+    Batch Normalization Module
 """
 
-import tensorflow.compat.v1 as tf
+import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
         Creates the operation to perform the batch normalization
+
+        Args:
+            Z: numpy.ndarray of shape (m, n) that contains the output of
+                the previous operation
+            gamma: numpy.ndarray of shape (1, n) containing the scales
+            beta: numpy.ndarray of shape (1, n) containing the offsets
+            epsilon: small number used to avoid division by zero
+
+        Returns:
+            Z_BN: the Batch Normalization of Z
     """
 
-    mean, var = tf.nn.moments(tf.cast(Z, tf.float16), axes=[0])
+    Z_BN = (Z - np.mean(Z, axis=0)) / (np.sqrt(np.var(Z, axis=0) + epsilon))
+    Z_BN = gamma * Z_BN + beta
 
-    return tf.nn.batch_normalization(Z, mean, var, beta, gamma, epsilon)
+    return Z_BN
