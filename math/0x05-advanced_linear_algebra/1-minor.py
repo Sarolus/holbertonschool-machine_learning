7b1fd24c302848d7cc70c1e2f09bccb0f7b34120
Sarolus <cactussius.primus@gmail.com>
2022-07-20 11:52:38 +0200

    Add matrix minor calculation task
    Co-authored-by: Adrien MILLOT <adrienmillot@users.noreply.github.com>

---
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+"""
+    Matrix Minor Calculation Module
+"""
+
+
+def get_matrix_minor(matrix, row, column):
+    """
+        Get the minor sub matrix.
+
+        Args:
+            matrix (list): The given matrix.
+            row (int): The row index.
+            column (int): The column index.
+
+        Returns:
+            list: The minor sub matrix.
+    """
+    return [
+        row[:column] + row[column + 1:]
+        for row in (matrix[:row] + matrix[row + 1:])
+    ]
+
+
+def create_sub_matrix(matrix_length, matrix, row_index):
+    """
+        Create the sub_matrix based on the given matrix.
+
+        Args:
+            matrix_length (int): Length of the given matrix.
+            matrix (list): The given matrix
+            row_index (int): Index of the length of the given matrix.
+
+        Returns:
+            list: The sub_matrix needed for the sub_determinant.
+    """
+    sub_matrix = [
+        [
+            matrix[row + 1]
+            [column if column < row_index else column + 1]
+            for column in range(matrix_length - 1)
+        ]
+        for row in range(matrix_length - 1)
+    ]
+
+    return sub_matrix
+
+
+def determinant(matrix):
+    """
+        Calculates the determinant of the given matrix.
+
+        Args:
+            matrix (list): The given matrix.
+
+        Returns:
+            int: The determinant of the given matrix.
+    """
+    matrix_length = len(matrix)
+    result = 0
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        return 1
+
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a list of lists')
+
+        if len(row) != matrix_length:
+            raise ValueError('matrix must be a square matrix')
+
+    if matrix_length == 1:
+        return matrix[0][0]
+
+    for row_index in range(matrix_length):
+        value = matrix[0][row_index]
+        sub_matrix = create_sub_matrix(matrix_length, matrix, row_index)
+        sub_determinant = determinant(sub_matrix)
+        result += ((-1) ** row_index) * value * sub_determinant
+
+    return result
+
+
+def minor(matrix):
+    """
+        Calculates the minor of the given matrix.
+
+        Args:
+            matrix (list): The given matrix.
+
+        Returns:
+            int: The minor of the given matrix.
+    """
+    matrix_length = len(matrix)
+    result = []
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a list of lists')
+
+        if len(row) != matrix_length:
+            raise ValueError('matrix must be a non-empty square matrix')
+
+    sub_result = []
+    if matrix_length == 1:
+        sub_result.append([1])
+        return sub_result
+
+    for row_index in range(matrix_length):
+        sub_det = []
+        for column_index in range(matrix_length):
+            sub_matrix = get_matrix_minor(matrix, row_index, column_index)
+            sub_determinant = determinant(sub_matrix)
+            sub_det.append(sub_determinant)
+        result.append(sub_det)
+
+    return result
