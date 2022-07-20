ca5b2bd94eb9c6f5318302bdcb40ad13311935e8
Sarolus <cactussius.primus@gmail.com>
2022-07-20 15:34:19 +0200

    Add matrix inverse calculation task

---
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    Matrix Inverse Calculation Module
+"""
+determinant = __import__('0-determinant').determinant
+adjugate = __import__('3-adjugate').adjugate
+
+
+def inverse(matrix):
+    """
+        Calculates the inverse matrix.
+
+        Args:
+            matrix (list): The given matrix.
+
+        Returns:
+            list: The inverse matrix.
+    """
+    det = determinant(matrix)
+    adj = adjugate(matrix)
+
+    matrix_length = len(adj)
+
+    if det == 0:
+        return None
+
+    inv = [
+        [adj[row][column] / det for column in range(matrix_length)]
+        for row in range(matrix_length)
+    ]
+
+    return inv
