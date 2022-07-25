7c06f554c1d5832557241fe01ea5c017c916b6ef
Sarolus <cactussius.primus@gmail.com>
2022-07-25 17:03:47 +0200

    Update multinormal class with Probability Density Function and fix mean_cov function

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -44,8 +44,37 @@ class MultiNormal:
         if dimensions < 2:
             raise ValueError("data must contain multiple data points")
 
-        mean = np.mean(X, axis=1).reshape(data_points, 1)
-        X -= mean
-        covariance = np.dot(X, X.T) / (dimensions - 1)
+        mean = np.mean(X.T, axis=0).reshape(1, data_points).T
+        X = X.T - mean.T
+        covariance = np.dot(X.T, X) / (dimensions - 1)
 
         return mean, covariance
+
+    def pdf(self, x):
+        """
+            Calculates the Probability Density Function
+
+            Args:
+                x (np.ndarray): np.ndarray containing the data points.
+
+            Returns:
+                pdf: Float containing the probability density
+                function
+        """
+
+        if not type(x) is np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+
+        if x.ndim != 2 or x.shape != (self.cov.shape[0], 1):
+            raise ValueError(
+                "x must have the shape ({}, 1)".format(self.cov.shape[0]))
+
+        k = self.cov.shape[0]
+        det = np.linalg.det(self.cov)
+        x -= self.mean
+
+        mul = np.dot(np.dot(x.T, np.linalg.inv(self.cov)), x)
+        exp = np.exp(-0.5 * mul)
+        pdf = exp / np.sqrt((np.pi * 2) ** k * det)
+
+        return np.asscalar(pdf)
