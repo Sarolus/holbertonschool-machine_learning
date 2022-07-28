6b18091a89f0deca63f19d6758e661eb0ea3dbc5
Sarolus <cactussius.primus@gmail.com>
2022-07-28 14:40:53 +0200

    Fix conditions

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -20,6 +20,24 @@ def posterior(x, n, p1, p2):
         Returns:
             posterior: posterior probability of x
     """
+    if type(n) is not int or n < 1:
+        raise ValueError("n must be a positive integer")
+
+    if type(x) is not int or x < 0:
+        text = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(text)
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(p1, float) or p1 < 0 or p1 > 1:
+        raise ValueError("p1 must be a float in the range [0, 1]")
+
+    if not isinstance(p2, float) or p2 < 0 or p2 > 1:
+        raise ValueError("p2 must be a float in the range [0, 1]")
+
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
 
     alpha = x + 1
     beta = n - x + 1
