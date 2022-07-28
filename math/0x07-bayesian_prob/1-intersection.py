45d442a3b8737644b43b510da3cdc1a481bb35bb
Sarolus <cactussius.primus@gmail.com>
2022-07-28 14:37:22 +0200

    Fix conditions

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -5,8 +5,6 @@
 
 import numpy as np
 
-likelihood = __import__('0-likelihood').likelihood
-
 
 def intersection(x, n, P, Pr):
     """
@@ -22,16 +20,41 @@ def intersection(x, n, P, Pr):
             intersection: intersection of the posterior and prior distributions
     """
 
-    # Calculate the likelihood of the data given the model
-    L = likelihood(x, n, P)
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
 
-    if not isinstance(Pr, int) and P.shape != Pr.shape:
-        raise TypeError("Pr must bbe a numpy.ndarray with the same shape as P")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
-    if any(i < 0 or i > 1 for i in Pr):
-        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
 
-    if np.isclose(np.sum(Pr), 1) is False:
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    for p in P:
+        if p < 0 or p > 1:
+            raise ValueError(
+                "All values in P must be in the range [0, 1]"
+            )
+
+    for prior in Pr:
+        if prior < 0 or prior > 1:
+            raise ValueError(
+                "All values in Pr must be in the range [0, 1]"
+            )
+
+    if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
+
+    factorial_p = np.math.factorial(
+        n) / (np.math.factorial(x) * np.math.factorial(n - x))
+
+    L = factorial_p * np.power(P, x) * np.power((1 - P), (n - x))
 
     return Pr * L
