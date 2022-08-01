75e4304ae3523c7ed92f4cb4e1146df7507d5c3b
Sarolus <cactussius.primus@gmail.com>
2022-08-01 16:49:37 +0200

    Add README.md & centroids cluster initialization

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    K-means Cluster Centroids Initialization
+"""
+
+import numpy as np
+
+
+def initialize(X, k):
+    """
+        Initializes cluster centroids for K-means
+
+        Args:
+            X: numpy.ndarray of shape (n, d) containing the dataset
+            k: int representing the number of clusters
+        Returns:
+            centroids: numpy.ndarray of shape (k, d) containing the centroids
+    """
+
+    _, dimensions = X.shape
+    min_value = np.ndarray.min(X, axis=0)
+    max_value = np.ndarray.max(X, axis=0)
+    # Draw samples from a uniform distribution.
+    # Samples are uniformly distributed over the half-open interval [low, high)
+    # (includes low, but excludes high)
+    centroids = np.random.uniform(
+        low=min_value, high=max_value, size=(k, dimensions))
+
+    return centroids
