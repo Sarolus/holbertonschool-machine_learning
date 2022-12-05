e150fe49b014f01491e76d1a29828173191c97d4
Sarolus <cactussius.primus@gmail.com>
2022-12-05 16:03:18 +0100

    Fix task 21

--- pipeline/0x02-databases/21-div.sql
+++ pipeline/0x02-databases/21-div.sql
@@ -1,19 +1,16 @@
 -- Creates a function that divides the first number by the second or returns 0 if the second number is 0
 
-DROP FUNCTION IF EXISTS SafeDiv;
 
-DELIMITER $$
+DELIMITER //
+
 CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS DOUBLE
-
+RETURNS FLOAT
+DETERMINISTIC
 BEGIN
-    DECLARE result DOUBLE;
-    IF b = 0 THEN
-	SET result = 0;
-    ELSE
-	SET result = a / b;
-    END IF;
-    RETURN result;
-END $$
-
+        IF b = 0 THEN
+           RETURN 0;
+        ELSE
+                RETURN (a / b);
+        END IF;
+END //
 DELIMITER ;
