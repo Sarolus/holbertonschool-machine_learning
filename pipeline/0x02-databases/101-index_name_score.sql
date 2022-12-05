1955aff04e5d5780c4df28cf1d053daf4debfc9f
Sarolus <cactussius.primus@gmail.com>
2022-12-05 16:48:54 +0100

    Fix task 36

--- pipeline/0x02-databases/101-index_name_score.sql
+++ pipeline/0x02-databases/101-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Creates an index on the table `names` and the first letter of `name` and the score
 
-CREATE INDEX names_first_letter_score_idx ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
