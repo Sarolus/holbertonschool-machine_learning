2a117db698a4818173b89aa9afc3cd8391fa9a39
Sarolus <cactussius.primus@gmail.com>
2022-12-05 15:54:46 +0100

    Fix task 14

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
 );
